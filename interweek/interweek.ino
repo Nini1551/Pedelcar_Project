5f0245c3744445682e4b31c699148db58cc312ca
Nini1551 <47613768+Nini1551@users.noreply.github.com>
2024-02-15 09:25:45 +0100

    update interweek.ino

--- interweek/interweek.ino
+++ interweek/interweek.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+#define TIMEOUT_DURATION 2000 // Timeout duration in milliseconds
 
 const int FRONT_TRIG_PIN = 5;
 const int FRONT_ECHO_PIN = 4;
@@ -6,10 +7,12 @@ const int LEFT_TRIG_PIN = 3;
 const int LEFT_ECHO_PIN = 2;
 const int RIGHT_TRIG_PIN = 7;
 const int RIGHT_ECHO_PIN = 6;
-const int FORWARD_MOTOR_PIN = 8;
-const int BACKWARD_MOTOR_PIN = 12;
+const int FORWARD_LED_PIN = 8;
+const int BACKWARD_LED_PIN = 12;
 const int SERVO_PIN = 9;
 const int BUTTON_PIN = 13;
+const int MOTOR_PIN = 10;
+const int BACKWARD_PIN = 11;
 
 const int ANGLE_MAX = 135;
 const int ANGLE_MIN = 45;
@@ -23,46 +26,75 @@ const int REFRESH_TIME = 50;
 
 const float SOUND_SPEED = 0.0343;
 
+Servo servo;
+
 int trig_pins[3] = {FRONT_TRIG_PIN, LEFT_TRIG_PIN, RIGHT_TRIG_PIN};
 int echo_pins[3] = {FRONT_ECHO_PIN, LEFT_ECHO_PIN, RIGHT_ECHO_PIN};
 char lib_pins[3][6] = {"Front", "Right", "Left"};
-char receivedChar;
 
 int trig_pin, echo_pin;
 float duration, distance;
 float distances[3];
 int avoid;
+int throttle = 0;
 
-Servo servo;
+char receivedChar;
+unsigned long lastReceiveTime = 0;
+bool disconnectedPrinted = false;
 
 void setup() {
-  
-  pinMode(FORWARD_MOTOR_PIN, OUTPUT);
-  pinMode(BACKWARD_MOTOR_PIN, OUTPUT);
+  pinMode(FORWARD_LED_PIN, OUTPUT);
+  pinMode(BACKWARD_LED_PIN, OUTPUT);
+  pinMode(BACKWARD_PIN, OUTPUT);
+  digitalWrite(BACKWARD_PIN, HIGH); // to go forward, backward pin need to set high
+  pinMode(MOTOR_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
   for (int i=0; i<3; i++) {
     pinMode(trig_pins[i], OUTPUT);
     pinMode(echo_pins[i], INPUT);
   }
   servo.attach(SERVO_PIN);
   Serial.begin(9600);
+  Serial1.begin(9600);
 }
 
 void loop() {
-  if(digitalRead(BUTTON_PIN)==1) {
-    set_measures();
-    if (is_in_danger()) {
-      go_backward();
-    } else {
-      go_forward();
+  if (Serial1.available()) {
+    // Update last receive time
+    lastReceiveTime = millis();
+    // Reset the disconnection flag
+    disconnectedPrinted = false;
+
+    receivedChar = Serial1.read();
+    Serial.print(receivedChar);
+    if (receivedChar != '\n') {
+      if(is_button_active()) {
+        set_measures();
+        if (is_in_danger()) {
+          go_backward();
+        } else {
+          go_forward();
+        }
+      }
+      else if (digitalRead(BUTTON_PIN)==0){
+        turn_off_forward_motor(); 
+      }
+      delay(REFRESH_TIME);
+
+      } else if (receivedChar == '0') {
+        turn_off_backward_motor();
+      }
+    }
+  } else {
+    // Check for timeout
+    if (millis() - lastReceiveTime > TIMEOUT_DURATION && !disconnectedPrinted) {
+      Serial.println("Bluethoot Disconnected");
+      disconnectedPrinted = true;
     }
   }
-  else if (digitalRead(BUTTON_PIN)==0){
-    turn_off_forward_motor(); 
-  }
-  delay(REFRESH_TIME);
 }
 
-void set_measures() {
+void set_measures() { // Update distances detect by sensors
   for (int i=0; i<3; i++) {
     
     trig_pin = trig_pins[i];
@@ -84,19 +116,19 @@ void set_measures() {
   }
 }
 
-void go_right() {
+void go_right() { // Guide the wheel on the right direction
   servo.write(ANGLE_MAX);
 }
 
-void go_left() {
+void go_left() { // Guide the wheel on the left direction
   servo.write(ANGLE_MIN);
 }
 
-void go_straight() {
+void go_straight() { // Guide the wheel on straight
 	servo.write(ANGLE_FORWARD);
 }
 
-bool is_in_danger() {
+bool is_in_danger() { // If there is a obstacle near
   if (distances[0] < CRITICAL) {
   	return true;	
   }
@@ -106,7 +138,7 @@ bool is_in_danger() {
   return false;
 }
 
-int need_avoiding() {
+int need_avoiding() { // Return the index of the direction needed (0: straight, 1: left, 2: right)
   float front_distance = distances[0];
   float left_distance = distances[1]; 
   float right_distance = distances[2]; 
@@ -127,18 +159,18 @@ int need_avoiding() {
   return 0;
 }
 
-void go_backward() {
+void go_backward() { // Start the reverse launche of the car
   turn_off_forward_motor();
   go_straight();
   turn_on_backward_motor();
-  while (need_avoiding() and digitalRead(BUTTON_PIN)==1) {
+  while (need_avoiding() and is_button_active()) {
   	set_measures();
     delay(REFRESH_TIME);
   }
   turn_off_backward_motor();
 }
 
-void go_forward() {
+void go_forward() { // start the for
   turn_on_forward_motor();
   set_measures();
   avoid = need_avoiding();
@@ -152,18 +184,44 @@ void go_forward() {
   }
 }
 
-void turn_on_forward_motor() {
-  digitalWrite(FORWARD_MOTOR_PIN, HIGH);
+void turn_on_forward_motor() { // Demare la marche avant du moteur
+  digitalWrite(FORWARD_LED_PIN, HIGH);
+  
+  while (throttle < 255) {
+  	ramping_throttle();
+  }
 }
 
-void turn_off_forward_motor() {
-  digitalWrite(FORWARD_MOTOR_PIN, LOW);
+void turn_off_forward_motor() { // Arrete la marche avant du moteur
+  digitalWrite(FORWARD_LED_PIN, LOW);
+  
+  analogWrite(MOTOR_PIN,0);
+  delay(1000);
 }
 
-void turn_on_backward_motor() {
-  digitalWrite(BACKWARD_MOTOR_PIN, HIGH);
+void turn_on_backward_motor() { // Demarre la marche arrière du moteur
+  digitalWrite(BACKWARD_LED_PIN, HIGH);
+
+  turn_off_forward_motor();
+  digitalWrite(BACKWARD_PIN,LOW);
+  delay(1000);
+  turn_on_forward_motor();
 }
 
-void turn_off_backward_motor() {
-  digitalWrite(BACKWARD_MOTOR_PIN, LOW);
+void turn_off_backward_motor() { // Arrete la marche arrière du moteur
+  digitalWrite(BACKWARD_LED_PIN, LOW);
+  
+  turn_off_forward_motor();
+  digitalWrite(BACKWARD_PIN, HIGH);
+  delay(1000);
+}
+
+void ramping_throttle() { // Active petit à petit le throttle
+  throttle+=5;
+  analogWrite(MOTOR_PIN,throttle);
+  delay(1);
+}
+
+bool is_button_active() { // Vrai lorsque le signal est activé
+  return receivedChar == '1';
 }
